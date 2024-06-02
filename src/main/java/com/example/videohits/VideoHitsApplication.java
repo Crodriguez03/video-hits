b9544ef58bee5f6e84962d2c80922a8ca0ff02b7
carlos <carlos@carlos>
2024-06-02 23:19:21 +0200

    Subida inicial

---
+++ src/main/java/com/example/videohits/VideoHitsApplication.java
@@ -0,0 +1,13 @@
+package com.example.videohits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoHitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoHitsApplication.class, args);
+	}
+
+}
